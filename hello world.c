cb17c16d5e323e1192efbbe832e1696c0e0526fc
JiangM0 <1521377272zzr@gmail.com>
2026-03-05 19:26:35 +0800

    改版

--- hello world.c
+++ hello world.c
@@ -2,5 +2,9 @@
 
 int main() {
     printf("Hello World\n");
+    char input[100];
+    printf("Please enter something: ");
+    scanf("%s", input);
+    printf("You entered: %s\n", input);
     return 0;
 }
