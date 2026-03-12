14cb88879a86df15b1af08e1b0eea6270735d201
JiangM0 <1521377272zzr@gmail.com>
2026-03-12 09:09:09 +0800

    更新 hello world.c

--- hello world.c
+++ hello world.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World\n");
-    char input[100];
-    printf("Please enter something: ");
-    scanf("%s", input);
-    printf("You entered: %s\n", input);
+    // 输出 Hello World
+    printf("Hello World!\n");
+
+    // 定义一个字符数组，用来存字符串
+    char str[100];
+
+    // 输入字符串
+    printf("请输入一段文字：");
+    scanf("%s", str);
+
+    // 输出字符串
+    printf("你输入的是：%s\n", str);
+
     return 0;
 }
